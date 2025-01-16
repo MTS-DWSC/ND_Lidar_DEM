822d38e9552d281d1b4a81c8fc2e020dbd2839e6
MTS-DWSC <mtsullivan@usgs.gov>
2025-01-16 07:10:12 -0600

    Update Isolated_IDs.py

--- Isolated_IDs.py
+++ Isolated_IDs.py
@@ -2,6 +2,7 @@ import arcpy
 import pandas as pd
 import os
 import numpy as np
+import sqlite3
 
 def get_id_sjo(key):
     sr = arcpy.SpatialReference(4326)
@@ -51,23 +52,16 @@ def get_ids():
         MFID.append(row[2])
 
     conn.close()
-    print(arr_FID, MFID)
     return arr_FID, MFID
 
-
-# ----
-
+# Start Here
 project = arcpy.mp.ArcGISProject("CURRENT")
 project_folder = os.path.dirname(project.filePath)
+gdb = os.path.join(project_folder, 'Default.gdb')
+line_fin = os.path.join(gdb, "PolylineLayer")
+arcpy.env.workspace = gdb
 
-num_arr, mainfileFID_arr = get_ids()
-
-print(num_arr, mainfileFID_arr)
-
-
-# Start Here
-
-
+sr = arcpy.SpatialReference(4326)
 project = arcpy.mp.ArcGISProject("CURRENT")
 project_folder = os.path.dirname(project.filePath)
 directory = os.path.join(project_folder, 'ConvertedSHP')
@@ -108,120 +102,69 @@ for filename in os.listdir(directory):
 
                 distance = start_point.distanceTo(end_point)
                 filtered_df.at[index, 'distance'] = distance
+                
+                # ----
+                
+            # First, find the row with the minimum distance
+            lowest_gridcode_df = filtered_df.nsmallest(5, 'gridcode')
+            fid_of_min_value = lowest_gridcode_df.at[lowest_gridcode_df['distance'].idxmin(), 'Id']
 
-filtered_df = filtered_df.reset_index(drop=True)
-print(filtered_df)
-#print('done')
-#print(filtered_df[['Id', 'gridcode', 'distance']])
-# Putall rest in theloop
+            # ---- Conert to 25 lowest and closest to Point
 
-# Modify so it doesn't need erasure
+            lowest_ele_id = fid_of_min_value
 
-# Get the gridcode value for that row
-min_distance_index = filtered_df['distance'].idxmin()
+            # Sort the DataFrame by 'Rank' (distance) and reset the index
+            sorted_df = filtered_df.sort_values(by='distance', ascending=True).reset_index(drop=True)
 
-min_distance_gridcode = filtered_df.at[min_distance_index, 'gridcode']
+            # Find the index of the row where 'Id' == lowest_ele_id
+            index_of_row_start = sorted_df[sorted_df['Id'] == lowest_ele_id].index[0]
+  
+            # Get the 5 rows above and 5 rows below using iloc
+            start_index = max(index_of_row_start - 30, 0)  # Ensure we don't go out of bounds at the start
+            end_index = min(index_of_row_start + 31, len(sorted_df))  # Ensure we don't go out of bounds at the end
 
-# Now, filter the dataframe to only rows with that gridcode value
-filtered_gridcode_df = filtered_df[filtered_df['gridcode'] == min_distance_gridcode]
+            surrounding_rows = sorted_df.iloc[start_index:end_index].reset_index(drop=True)
+            surrounding_rows['Id_diff'] = abs(surrounding_rows['Id'] - lowest_ele_id).reset_index(drop = True)
 
-# Find the FID of the row with the minimum distance within that filtered set
-fid_of_min_value = filtered_gridcode_df.at[filtered_gridcode_df['distance'].idxmin(), 'Id']
+            # Find the row with the maximum difference
+            max_diff_row = surrounding_rows.loc[surrounding_rows['Id_diff'].idxmax()]
 
-print(fid_of_min_value)
+            # Extract the 'Id' value from that row
+            furthest_id = max_diff_row['Id']
+            index_of_row_end = sorted_df[sorted_df['Id'] == furthest_id].index[0]
 
+            # ----
 
-# ---- Conert to 25 lowest and closest to Point
+            end_point = sorted_df.iloc[index_of_row_end]['SHAPE@']
+            start_point = sorted_df.iloc[index_of_row_start]['SHAPE@']
+            spoint = extract_coordinates(start_point)
+            epoint = extract_coordinates(end_point)
 
-lowest_ele_id = fid_of_min_value
+            # ----
+            data_dict = {}
+            array = arcpy.Array([spoint, epoint])
+            line = arcpy.Polyline(array, sr)
+            data_dict[lowest_ele_id] = line
 
-# Sort the DataFrame by 'Rank' (distance) and reset the index
-sorted_df = filtered_df.sort_values(by='distance', ascending=True).reset_index(drop=True)
+            output_fc = os.path.join(gdb,"testline")
+            arcpy.CopyFeatures_management(line, output_fc)
+            # ----
+            
+            data_dict = {}
+            array = arcpy.Array([spoint, epoint])
+            line = arcpy.Polyline(array, sr)
+            data_dict[lowest_ele_id] = line
 
-# Find the index of the row where 'Id' == lowest_ele_id
-index_of_row_start = sorted_df[sorted_df['Id'] == lowest_ele_id].index[0]
+            with arcpy.da.InsertCursor(line_fin, ["SHAPE@", "FID"]) as cursor:
+                for key, val in data_dict.items():
+                    try:
+                        cursor.insertRow([val, key])
+                        print('Inserted.')
+                    except Exception as e:
+                        print(f"Error creating polyline for key {key} with value {val}: {e}")
+            
+            # ----
 
-# Get the 5 rows above and 5 rows below using iloc
-start_index = max(index_of_row_start - 30, 0)  # Ensure we don't go out of bounds at the start
-end_index = min(index_of_row_start + 31, len(sorted_df))  # Ensure we don't go out of bounds at the end
-
-surrounding_rows = sorted_df.iloc[start_index:end_index].reset_index(drop=True)
-surrounding_rows['Id_diff'] = abs(surrounding_rows['Id'] - lowest_ele_id).reset_index(drop = True)
-
-# Find the row with the maximum difference
-max_diff_row = surrounding_rows.loc[surrounding_rows['Id_diff'].idxmax()]
-
-# Extract the 'Id' value from that row
-furthest_id = max_diff_row['Id']
-index_of_row_end = sorted_df[sorted_df['Id'] == furthest_id].index[0]
-
-
-"""
-start_point = sorted_df.iloc[index_of_row_start]['SHAPE@']
-centroid_geom = arcpy.PointGeometry(start_point.centroid, start_point.spatialReference)
-projected_centroid = centroid_geom.projectAs(spatial_reference)
-point = arcpy.Point(projected_centroid.centroid.X, projected_centroid.centroid.Y)
-sp_point = arcpy.PointGeometry(point, spatial_reference)
-spoint = extract_coordinates(start_point)
-
-for index, row in surrounding_rows.iterrows():
-    cell = row['SHAPE@']
-    centroid_geom = arcpy.PointGeometry(cell.centroid, cell.spatialReference)
-    projected_centroid = centroid_geom.projectAs(spatial_reference)
-
-    point = arcpy.Point(projected_centroid.centroid.X, projected_centroid.centroid.Y)
-    end_point = arcpy.PointGeometry(point, spatial_reference)
-
-    distance = sp_point.distanceTo(end_point)
-    surrounding_rows.at[index, 'distance_end_point'] = distance
-
-print(surrounding_rows[['Id', 'gridcode', 'distance', 'distance_end_point']])
-
-# Sort by 'distance to the point centered around' (Column A) in ascending order
-sorted_df = surrounding_rows.sort_values(by='distance', ascending=True)
-
-# Further sort by 'distance to the point looking at' (Column B) in descending order
-sorted_df = surrounding_rows.sort_values(by='distance_end_point', ascending=False)
-
-print(sorted_df[['Id', 'gridcode', 'distance', 'distance_end_point']])
-
-"""
-end_point = sorted_df.iloc[index_of_row_end]['SHAPE@']
-start_point = sorted_df.iloc[index_of_row_start]['SHAPE@']
-spoint = extract_coordinates(start_point)
-epoint = extract_coordinates(end_point)
-
-print(spoint, epoint)
-
-## Need to update this for lowest on 2nd half
-
-"""
-data_dict = {}
-array = arcpy.Array([spoint, epoint])
-line = arcpy.Polyline(array, sr)
-data_dict[lowest_ele_id] = line
-
-output_fc = os.path.join(gdb,"testline")
-arcpy.CopyFeatures_management(line, output_fc)
-"""
-
-id = 999
-
-project = arcpy.mp.ArcGISProject("CURRENT")
-project_folder = os.path.dirname(project.filePath)
-gdb = os.path.join(project_folder, 'Default.gdb')
-line_fin = os.path.join(gdb, "PolylineLayer")
-arcpy.env.workspace = gdb
-
-spatial_reference = arcpy.SpatialReference(4326)
-
-with arcpy.da.InsertCursor(line_fin, ["SHAPE@", "FID"]) as cursor:
-    for key, val in data_dict.items():
-        try:
-            cursor.insertRow([val, key])
-            print('Inserted.')
-        except Exception as e:
-            print(f"Error creating polyline for key {key} with value {val}: {e}")
-
-print('done')
-
+            
+print(sorted_df)     
+print('done')             
